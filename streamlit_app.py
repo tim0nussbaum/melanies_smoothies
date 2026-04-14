2080dbbc28ebaae478404e970fdbf515cf918f64
Tim Nussbaum <tim@mapletonhill.net>
2026-04-14 14:46:26 -0600

    Unitlisze search_on and clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit_name"), col("search_on"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -42,7 +37,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
